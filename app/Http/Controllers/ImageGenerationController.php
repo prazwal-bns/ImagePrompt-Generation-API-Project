6a724aca1b786070ef4d54a59190d072ee5ca2ba
prazwal-bns <prajwalbns15@gmail.com>
2025-11-09 19:56:24 +0545

    Add ImageGenerationController with basic CRUD methods

---
+++ app/Http/Controllers/ImageGenerationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageGenerationController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show(ImageGeneration $imageGeneration)
+    {
+
+    }
+
+    public function update(Request $request, ImageGeneration $imageGeneration)
+    {
+
+    }
+    
+    public function destroy(ImageGeneration $imageGeneration)
+    {
+
+    }
+}
