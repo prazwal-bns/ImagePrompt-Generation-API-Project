ca2f46d812b8efa7228127daf857d42b0132ed1a
prazwal-bns <prajwalbns15@gmail.com>
2025-11-10 12:20:07 +0545

    Add image upload functionality in ImageGenerationController, including sanitization of file names and storage in public directory.

--- app/Http/Controllers/ImageGenerationController.php
+++ app/Http/Controllers/ImageGenerationController.php
@@ -14,6 +14,17 @@ class ImageGenerationController extends Controller
 
     public function store(GeneratePromptRequest $request)
     {
+        $user = $request->user();
 
+        $image = $request->file('image');
+
+        $originalFileName = $image->getClientOriginalName();
+        // Remove any special characters from the file name for eg: "Hello World.png" to "Hello_World.png"
+        $sanitizedName = preg_replace('/[^a-zA-Z0-9]/', '_', pathinfo($originalFileName, PATHINFO_FILENAME));
+
+        $extension = $image->getClientOriginalExtension();
+        $safeFileName = $sanitizedName . '_' . time() . '.' . $extension;
+
+        $image->storeAs('uploads/images', $safeFileName, 'public');
     }
 }
