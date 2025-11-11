fae8d9cd75a9394c31b9d16ad8652489b0e4a055
prazwal-bns <prajwalbns15@gmail.com>
2025-11-11 08:27:50 +0545

    Configured Scramble to scan only API routes and added bearer token authentication to the OpenAPI documentation.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Str;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,6 +26,19 @@ class AppServiceProvider extends ServiceProvider
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        // Configure Scramble to only scan API routes
+        Scramble::configure()
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+        });
+
+        // Add bearer token authentication to the API documentation
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::http('bearer', 'BearerAuth')
+            );
         });
     }
 }
